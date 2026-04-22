09e747ceca112abc9dc0b8fb619239c6d37e2fb9
guilhermefragoso21 <pereira.sousa.guilherme@escola.pr.gov.br>
2026-04-22 14:15:04 -0300

    0

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var LOGO_SRC = "";
-var BG_SRC = "";
+var LOGO_SRC = "imagens/Logo PLAIN.png";    
+var BG_SRC   = "imagens/fundosite.png";
 
 function initImages() {
   var logo = document.getElementById("logo-img");
